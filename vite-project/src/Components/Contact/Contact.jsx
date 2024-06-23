7be50d33881ea1b538674cebc890080801bb4594
kyber03 <102473674+kyber03@users.noreply.github.com>
2024-06-23 19:32:26 +0530

    added media query for navbar with open and close icon visible on media width small screen . used useref() added functions open menu and closemenu

--- vite-project/src/Components/Contact/Contact.jsx
+++ vite-project/src/Components/Contact/Contact.jsx
@@ -6,6 +6,29 @@ import locationicon from "../../assets/location_icon.svg";
 import callicon from "../../assets/call_icon.svg";
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "c4f7f05c-9a34-48d2-8481-1de0f7a3985e");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="c-title">
@@ -35,10 +58,10 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <form action="" className="right-s">
+        <form onSubmit={onSubmit} action="" className="right-s">
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder="Enter your name" name="name" />
-          <label htmlFor="">Your Name</label>
+          <label htmlFor="">Your Email</label>
           <input type="email" placeholder="Enter your mail" name="email" />
           <label htmlFor="">Write your Message here</label>
           <textarea name="" id="" rows="8"></textarea>
--- vite-project/src/Components/Navbar/navbar.jsx
+++ vite-project/src/Components/Navbar/navbar.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import toggle_m from "../../assets/menu_open.svg";
+import toggle_menuclose from "../../assets/menu_close.svg";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const menuref = useRef();
+
+  const openmenu = () => {
+    menuref.current.style.right = "0";
+  };
+  const closemenu = () => {
+    menuref.current.style.right = "-350px";
+  };
   return (
     <div className="navbar">
       <img src={logo} alt="" />
-      <ul className="nav-menu">
+      <img onClick={openmenu} src={toggle_m} alt="" className="nav-mob-open" />
+      <ul ref={menuref} className="nav-menu">
+        <img
+          onClick={closemenu}
+          src={toggle_menuclose}
+          alt=""
+          className="nav-mob-close"
+        />
         <li>
           <AnchorLink className="anchor-link" href="#home">
             <p onClick={() => setMenu("home")}>Home</p>
